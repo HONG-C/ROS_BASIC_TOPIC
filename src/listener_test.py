b050ddf2dcb932ce14e864478d7dfbff68f846a4
HONG-C <jihun3900@naver.com>
2021-06-30 14:49:20 +0900

    listener_test.py linear에서 float64값인 x값 추출 완료,이유를모르겠어서 추가적인연구 필요

--- src/listener_test.py
+++ src/listener_test.py
@@ -6,13 +6,13 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 def callback(data):
-      rospy.loginfo(data.linear)
+      rospy.loginfo(rospy.get_caller_id() + "I heard %f",data.linear.x)
 
 def listener_test():
       rospy.init_node('listener_test',anonymous=True)
 
-      #rospy.Subscriber("chatter", String, callback)
       rospy.Subscriber("turtle1/cmd_vel",Twist, callback)
+
       rospy.spin()
 
 if __name__=='__main__':

61a7d5d9435a573725169753aa9756eaafb3a501
HONG-C <jihun3900@naver.com>
2021-07-19 23:36:10 +0900

    ros메시지 파일 만드는법 적용 성공,성공해서 다행..

--- src/steering_angle_speed.py
+++ src/steering_angle_speed.py
@@ -3,15 +3,18 @@
 
 import rospy	#ros 내에서 파이썬 사용을 위한 파일 
 from std_msgs.msg import Int64#기본 메시지 파일 불러오기 
+from ros_basic_topic.msg import Num#직접 만든 메시지 파일 불러오기(ros_basic_topic패키지에 메시지 파일 있고,Num파일을 임포트) 
 
 def read_angle():
-      pub = rospy.Publisher('angle',Int64,queue_size=10)
-      rospy.init_node('steering_angle', anonymous = True)
+      car_data=Num()
+      pub = rospy.Publisher('car_data',Num,queue_size=10)
+      rospy.init_node('steering_angle_speed', anonymous = True)
       rate = rospy.Rate(10) #10hz
       while not rospy.is_shutdown():
-      	    angle=input("조향각 얼마?:")
-            rospy.loginfo(angle)
-            pub.publish(angle)
+      	    car_data.angle=input("조향각 얼마?:")
+      	    car_data.speed=input("속도 얼마?:")
+            rospy.loginfo(car_data)
+            pub.publish(car_data)
             rate.sleep()
 
 if __name__=='__main__': #이 구문은 뭘까?import된 경우가 아니라 인터프리터에서 직접 실행된 경우만 if문 이하 실행하기!
---
+++ src/steering_speed_result .py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy	#ros 내에서 파이썬 사용을 위한 파일 
+from std_msgs.msg import Int64#기본 메시지 파일 불러오기 
+from ros_basic_topic.msg import Num#직접 만든 메시지 파일 불러오기(ros_basic_topic패키지에 메시지 파일 있고,Num파일을 임포트) 
+
+
+def callback(data):
+      rospy.loginfo(rospy.get_caller_id() + "I heard %d", data.angle)
+      rospy.loginfo(rospy.get_caller_id() + "I heard %d", data.speed)
+
+def print_angle():
+      rospy.init_node('car_data_result',anonymous=True) #노드명 초기화 
+
+      rospy.Subscriber("car_data", Num, callback)
+
+      rospy.spin()
+
+if __name__=='__main__':
+      print_angle()
+
+
+

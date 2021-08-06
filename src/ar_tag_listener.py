20c111c4d1d9bfd633401da350b00217ad330e17
HONG-C <jihun3900@naver.com>
2021-08-06 13:10:02 +0900

    ar_pose_marker에서 id얻는 것 성공! type을 통해 형식 확인 후 리스트에서 추출 후 문자열로 변환하여 인덱싱

--- src/ar_tag_listener.py
+++ src/ar_tag_listener.py
@@ -6,8 +6,13 @@ from std_msgs.msg import String#기본 메시지 파일 불러오기
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
 def callback(data):
-      rospy.loginfo(data.markers)
-
+      if len(data.markers)==0:#ar_tag의 값은 리스트로 나오므로, 아무것도 없을 시에는 처리 x
+	      rospy.loginfo("NO_AR_TAG")
+      if len(data.markers)==1:#ar_tag가 하나 인식될시 처리 
+	      id_num=str(data.markers).split('\n')[6]#ar_pose_marker을 문자열 형식으로 변환 후 슬라이싱
+      	      rospy.loginfo(id_num)
+      else:
+	      pass
 def listener():
       rospy.init_node('listener',anonymous=True) #노드명 초기화 
 

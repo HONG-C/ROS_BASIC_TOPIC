4cce34bb4a5dacb39c668cfb3b1a8a4df3df1ce3
HONG-C <jihun3900@naver.com>
2021-08-15 13:30:32 +0900

    노드 수정스크립트 추가

--- src/basic_node_2/steering_result.py
+++ src/basic_node_2/steering_result.py
@@ -5,7 +5,7 @@ import rospy	#ros 내에서 파이썬 사용을 위한 파일
 from std_msgs.msg import Int64#기본 메시지 파일 불러오기 
 
 def callback(data):
-      rospy.loginfo(rospy.get_caller_id() + "I heard %d", data.data)
+      rospy.loginfo(rospy.get_caller_id() + "모터값 출력: %d", data.data)
 
 def print_angle():
       rospy.init_node('steering_result',anonymous=True) #노드명 초기화 

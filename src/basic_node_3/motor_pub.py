18e92b20a927e2825d40988c2f8770da8b6780a8
HONG-C <jihun3900@naver.com>
2021-08-23 16:38:39 +0900

    새로운 노드 추가

---
+++ src/basic_node_3/motor_pub.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy	#ros 내에서 파이썬 사용을 위한 파일 
+from std_msgs.msg import Int64#기본 메시지 파일 불러오기 
+#float 64형의 경우에는 하위 데이터 data가 있음 
+
+motor_angle=0
+
+def callback(data):
+      global motor_angle
+      rospy.loginfo(rospy.get_caller_id() + "모터값 출력: %d", data.data)
+      motor_angle=data.data
+
+def print_angle():
+      global motor_angle
+      rospy.init_node('motor_pub',anonymous=True) #노드명 초기화 
+      pub = rospy.Publisher('your_topic',Int64,queue_size=10)
+      rospy.Subscriber("angle", Int64, callback)
+      rate = rospy.Rate(10) #default is 10hz
+      while not rospy.is_shutdown():
+      	    pub.publish(motor_angle)
+            rate.sleep()
+      rospy.spin()
+
+if __name__=='__main__':
+      print_angle()
+
+
+
---
+++ src/basic_node_3/steering.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy	#ros 내에서 파이썬 사용을 위한 파일 
+from std_msgs.msg import Int64#기본 메시지 파일 불러오기 
+
+def read_angle():
+      pub = rospy.Publisher('angle',Int64,queue_size=10)
+      rospy.init_node('steering', anonymous = True)
+      rate = rospy.Rate(10) #10hz
+      while not rospy.is_shutdown():
+      	    angle=input("조향각 얼마?:")
+            rospy.loginfo(angle)
+            pub.publish(angle)
+            rate.sleep()
+
+if __name__=='__main__': #이 구문은 뭘까?import된 경우가 아니라 인터프리터에서 직접 실행된 경우만 if문 이하 실행하기!
+      try:
+            read_angle()
+      except rospy.ROSInterruptException:
+            pass
+
